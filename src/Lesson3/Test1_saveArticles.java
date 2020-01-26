e7eddcc6015425d8f73cf28b2d7703fc325e14df
aleksss90 <uae387@gmail.com>
2020-01-26 23:06:44 +0300

    other_files

--- src/Lesson3/Test1_saveArticles.java
+++ src/Lesson3/Test1_saveArticles.java
@@ -22,27 +22,28 @@ public class Test1_saveArticles {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platformVersion", "6.0");
+        capabilities.setCapability("platformVersion", "9");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "C:/Users/Alex/Desktop/AutomationTests/apks/org.wikipedia.apk");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-        waitForElementAndClick(
+/*        waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Skip')]"),
                 "Cannot find Skip button",
                 10
-        );
+        );*/
     }
 
     @After
     public void tearDown() {
         driver.quit();
     }
+
     @Test
     public void cancellationSearch() {
-        String name1_of_folder ="Kotlin (programming language)";
-        String name2_of_folder ="Football";
+        String name1_of_folder = "Kotlin (programming language)";
+        String name2_of_folder = "Football";
 
         waitForElementAndClick(
                 By.id("org.wikipedia:id/search_container"),
@@ -56,13 +57,12 @@ public class Test1_saveArticles {
                 5
         );
         waitForElementAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='"+name1_of_folder+"']"),
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + name1_of_folder + "']"),
                 "Cannot find article Kotlin",
                 10
         );
-        //!!!
         waitForElementPresent(
-                By.xpath("//*[@content-desc='"+name1_of_folder+"']"),
+                By.xpath("//*[@content-desc='" + name1_of_folder + "']"),
                 "Cannot find article Kotlin in page",
                 10
         );
@@ -109,12 +109,12 @@ public class Test1_saveArticles {
                 5
         );
         waitForElementAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='"+name2_of_folder+"']"),
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + name2_of_folder + "']"),
                 "Cannot find article Football",
                 10
         );
         waitForElementPresent(
-                By.xpath("//*[@content-desc='"+name2_of_folder+"']"),
+                By.xpath("//*[@content-desc='" + name2_of_folder + "']"),
                 "Cannot find article Football in page",
                 10
         );
@@ -145,62 +145,60 @@ public class Test1_saveArticles {
                 5
         );
         swipeElementToLeft(
-                By.xpath("//*[@text='"+name1_of_folder+"']"),
+                By.xpath("//*[@text='" + name1_of_folder + "']"),
                 "Cannot swipe to delete Kotlin (programming language)"
         );
 
         waitForElementNotPresent(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='"+name1_of_folder+"']"),
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + name1_of_folder + "']"),
                 "not delete article Kotlin (programming language)",
                 5
         );
         waitForElementPresent(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='"+name2_of_folder+"']"),
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + name2_of_folder + "']"),
                 "article Football is still on saved list",
                 5
         );
         waitForElementAndClick(
-                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='"+name2_of_folder+"']"),
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='" + name2_of_folder + "']"),
                 "Cannot find article Football",
                 10
         );
         waitForElementPresent(
-                By.xpath("//*[@content-desc='"+name2_of_folder+"']"),
+                By.xpath("//*[@content-desc='" + name2_of_folder + "']"),
                 "Cannot find article Football in page",
                 10
         );
     }
-        private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-            wait.withMessage(error_message + "\n");
-            return wait.until(
-                    ExpectedConditions.presenceOfElementLocated(by)
-            );
-        }
 
-        private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds)
-        {
-            WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
-            element.click();
-            return element;
-        }
-        private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds)
-        {
-            WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
-            element.sendKeys(value);
-            return element;
-        }
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
 
-        private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds)
-        {
-            WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-            wait.withMessage(error_message + "\n");
-            return wait.until(ExpectedConditions.invisibilityOfElementLocated(by)
-            );
-        }
-    protected void swipeElementToLeft(By by, String error_message)
-    {
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+
+    protected void swipeElementToLeft(By by, String error_message) {
         WebElement element = waitForElementPresent(
                 by,
                 error_message,
